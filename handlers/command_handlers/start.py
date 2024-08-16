93931a8bc9ea3fdbd59ad5c19a86ef9a9fad356e
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-08-16 15:50:35 +0400

    feat: handle start command

---
+++ handlers/command_handlers/start.py
@@ -0,0 +1,15 @@
+import telebot.types
+
+from loader import bot
+from states.user_states import UserStates
+import messages
+
+
+@bot.message_handler(commands=['start'])
+def start(message: telebot.types.Message):
+	bot.set_state(message.chat.id, UserStates.start)
+	bot.send_message(
+		message.chat.id,
+		messages.dialogue.start_dialogue(message),
+		parse_mode='html'
+	)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from loader import bot
+import handlers  # noqa
 
 if __name__ == '__main__':
 	bot.infinity_polling()
