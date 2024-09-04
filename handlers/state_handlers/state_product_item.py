503ae22bd628f0012551f109e8ef07ce7a0f5bd0
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-09-04 19:09:53 +0300

    feat: send product card

--- handlers/state_handlers/state_product_item.py
+++ handlers/state_handlers/state_product_item.py
@@ -23,3 +23,22 @@ def get_product_info(message: telebot.types.Message):
 
 	with bot.retrieve_data(message.from_user.id) as data:
 		data[product_item] = product_data
+
+	sizes = product_data.get('sizes')
+
+	if len(sizes) == 1:  # У товара только один размер
+		product_card = messages.product_card.ProductCard(
+			product_item=product_item, product_data=product_data, size_name=''
+		)
+	else:
+		product_card = messages.product_card.ProductCard(
+			product_item=product_item, product_data=product_data,
+		)
+
+	bot.send_photo(
+		message.chat.id,
+		product_card.image_link,
+		product_card.caption(),
+		parse_mode='html',
+		reply_markup=product_card.markup()
+	)
