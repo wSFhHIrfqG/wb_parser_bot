0f16b97fe75c669f76db7788a02796e8f1fef5e7
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-09-05 19:54:26 +0300

    feat: handle size chosen

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,2 +1,3 @@
 from . import command_handlers
 from . import state_handlers
+from . import callback_handlers
---
+++ handlers/callback_handlers/size_chosen.py
@@ -0,0 +1,23 @@
+from loader import bot
+import messages
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('size'))
+def size(call):
+	product_item = int(call.data.split(':')[1])
+	size_name = call.data.split(':')[2]
+
+	with bot.retrieve_data(call.message.chat.id) as data:
+		product_data = data[product_item]
+
+	product_card = messages.product_card.ProductCard(
+		product_item=product_item, product_data=product_data, size_name=size_name
+	)
+
+	bot.edit_message_caption(
+		product_card.caption(),
+		call.message.chat.id,
+		message_id=call.message.message_id,
+		parse_mode='html',
+		reply_markup=product_card.markup()
+	)
