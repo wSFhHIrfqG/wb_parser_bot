6387d7da3d9b3cf4e06c22f993fc1c2965604a26
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-09-07 11:25:33 +0300

    feat: create price checker

---
+++ price_checker.py
@@ -0,0 +1,68 @@
+from threading import Thread
+import time
+
+from loader import bot
+import parse
+import database
+import messages
+
+
+class PriceChecker(Thread):
+
+	def run(self):
+		time.sleep(60 * 10)
+		self.check_products()
+
+	def check_products(self):
+		for product in database.read.all_products():
+			_id, chat_id, product_item, size_name, old_price = product
+
+			product = parse.wb_product.WBProduct(product_item)
+			product_data = product.product_data()
+
+			actual_price = None
+			for size in product_data.get('sizes'):
+				if size.get('name') == size_name:
+					actual_price = size.get('price', {}).get('total')
+					break
+
+			product_card = messages.product_card.ProductCard(
+				product_item=product_item, product_data=product_data, size_name=size_name
+			)
+
+			if self.product_stocked(old_price, actual_price):  # Товар появился в наличии
+				msg = bot.send_message(
+					chat_id,
+					product_card.caption(),
+					parse_mode='html'
+				)
+				bot.reply_to(
+					msg,
+					'🛒 Товар появился в наличии!'
+				)
+			elif self.price_decrease(old_price, actual_price):  # Цена на товар снизилась
+				msg = bot.send_message(
+					chat_id,
+					product_card.caption(),
+					parse_mode='html'
+				)
+				bot.reply_to(
+					msg,
+					'🛒 Цена на товар снизилась!'
+					f'{old_price} ₽ ➡️ {actual_price} ₽'
+				)
+
+	@staticmethod
+	def product_stocked(old_price: int | None, actual_price: int | None):
+		"""Товар появился в наличии"""
+		if (old_price is None) and (actual_price is not None):
+			return True
+		return False
+
+	@staticmethod
+	def price_decrease(old_price: int | None, actual_price: int | None):
+		"""Цена на товар снизилась"""
+		if (old_price is not None) and (actual_price is not None):
+			if actual_price < old_price:
+				return True
+		return False
